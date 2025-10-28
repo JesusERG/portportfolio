03547434dda91d4082d45bac1c3009c6c98695d5
Jesus Rodriguez <Jesus.Rodriguez@techconsulting.tech>
2025-10-28 15:38:15 -0400

    Bare Bones Project

---
+++ app/about/Jackpot.tsx
@@ -0,0 +1,13 @@
+import JackpotItem from "./JackpotItem";
+
+const Jackpot = () => {
+  return (
+    <div>
+      <JackpotItem />
+      <JackpotItem />
+      <JackpotItem />
+    </div>
+  );
+};
+
+export default Jackpot;
---
+++ app/about/JackpotItem.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const JackpotItem = () => {
+  return <div></div>;
+};
+
+export default JackpotItem;
